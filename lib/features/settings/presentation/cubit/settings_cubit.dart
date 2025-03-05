deca92efda1efc679b39a406b4cee32f9c4dc1ff
Mohamed Yasser <mohamedhcjdivdjvy@gmail.com>
2025-03-05 21:06:06 +0200

    create profile repo and cubit

--- lib/features/settings/presentation/cubit/settings_cubit.dart
+++ lib/features/settings/presentation/cubit/settings_cubit.dart
@@ -1,8 +1,43 @@
-import 'package:bloc/bloc.dart';
+import 'dart:io';
+
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:marketi/features/settings/data/models/user_profile_model.dart';
+import 'package:marketi/features/settings/data/repositories/profile_repo.dart';
 
 part 'settings_state.dart';
 
 class SettingsCubit extends Cubit<SettingsState> {
-  SettingsCubit() : super(SettingsInitial());
+  final ProfileRepo profileRepo;
+
+  SettingsCubit({required this.profileRepo}) : super(SettingsState());
+
+  Future<void> getProfile() async {
+    emit(const SettingsState(status: SettingsStatus.loading));
+    final result = await profileRepo.getProfile();
+
+    result.when(
+      success: (data) => emit(SettingsState(
+          status: SettingsStatus.success, userProfileModel: data)),
+      error: (apiError) => emit(SettingsState(
+        status: SettingsStatus.error,
+        errorMessage: apiError.message,
+      )),
+    );
+  }
+
+  Future<void> updateProfile(
+      String name, String email, File? profileImage) async {
+    emit(const SettingsState(status: SettingsStatus.loading));
+    final result = await profileRepo.updateProfile(name, email, profileImage);
+
+    result.when(
+      success: (data) => emit(SettingsState(
+          status: SettingsStatus.success, userProfileModel: data)),
+      error: (apiError) => emit(SettingsState(
+        status: SettingsStatus.error,
+        errorMessage: apiError.message,
+      )),
+    );
+  }
 }
--- lib/features/settings/presentation/cubit/settings_state.dart
+++ lib/features/settings/presentation/cubit/settings_state.dart
@@ -1,10 +1,28 @@
 part of 'settings_cubit.dart';
 
-abstract class SettingsState extends Equatable {
-  const SettingsState();
+enum SettingsStatus { initial, loading, success, error }
+
+class SettingsState extends Equatable {
+  const SettingsState({
+    this.status = SettingsStatus.initial,
+    this.userProfileModel,
+    this.errorMessage,
+  });
+
+  final SettingsStatus status;
+  final String? errorMessage;
+  final UserProfileModel? userProfileModel;
 
   @override
-  List<Object> get props => [];
-}
+  List<Object?> get props => [status, errorMessage];
 
-class SettingsInitial extends SettingsState {}
+  SettingsState copyWith(
+      {SettingsStatus? status,
+      String? errorMessage,
+      UserProfileModel? userProfileModel}) {
+    return SettingsState(
+        status: status ?? this.status,
+        errorMessage: errorMessage ?? this.errorMessage,
+        userProfileModel: userProfileModel ?? this.userProfileModel);
+  }
+}
